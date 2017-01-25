be21f9952ba0471cc7914e73edecf8664ab3e8ce
Pierre Fernique <pfernique@gmail.com>
2017-01-25 09:21:10 +0100

    Add patch

--- before_build.bat
+++ before_build.bat
@@ -1,3 +1,10 @@
 echo ON
 
+if not "%RECIPE%"=="" (
+	if exist ..\conda\%RECIPE%\appveyor-ci.patch (
+		git apply -v ..\conda\$RECIPE\appveyor-ci.patch
+		if errorlevel neq 0 exit /b 1
+	)
+)
+
 echo OFF
