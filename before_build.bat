bdb36394e4572e2280f7e6a2846bbc1dfc0a18cd
Pierre Fernique <pfernique@gmail.com>
2017-01-25 09:42:19 +0100

    Update before_build.bat

--- before_build.bat
+++ before_build.bat
@@ -5,9 +5,10 @@ dir
 if not "%RECIPE%"=="" (
 	dir ..\conda\%RECIPE%
 	if exist ..\conda\%RECIPE%\appveyor-ci.patch (
-		echo apply
+		cd ..
 		git apply -v ..\conda\%RECIPE%\appveyor-ci.patch
 		if errorlevel neq 0 exit /b 1
+		cd appveyor-ci
 	)
 )
 
