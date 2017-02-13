89293428c163ff33f96b11be3d6e370057d5b549
Pierre Fernique <pfernique@gmail.com>
2017-02-13 13:05:50 +0100

    Update build_script.bat

--- build_script.bat
+++ build_script.bat
@@ -1,6 +1,6 @@
 echo ON
 
-if not "%RECIPE%"=="" %CMD_IN_ENV% conda build ..\conda\%RECIPE% -c statiskit -c conda-forge
+if not "%RECIPE%"=="" conda build ..\conda\%RECIPE% -c statiskit -c conda-forge
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo OFF
