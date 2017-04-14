f413e9379645745af92b56a61a1b6e3d4d3a4c49
Pierre Fernique <pfernique@gmail.com>
2017-04-14 10:14:28 +0200

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -1,13 +1,10 @@
 echo OFF
 
-dir .
 git clone https://github.com/StatisKit/StatisKit.git
 if exist StatisKit\doc\win_%INSTALL%.exe (
-  dir StatisKit\doc
   del StatisKit\doc\win_%INSTALL%.exe
 )
-move $INSTALL.exe StatisKit\doc\win_%INSTALL%.exe
-dir StatisKit\doc
+move %INSTALL%.exe StatisKit\doc\win_%INSTALL%.exe
 cd StatisKit
 git config --global user.email %GIT_EMAIL%
 git config --global user.name %GIT_NAME%
