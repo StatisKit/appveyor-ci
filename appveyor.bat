fbaed6400beda76bb11fa807dd39cfec028b9234
Pierre Fernique <pfernique@gmail.com>
2017-04-14 08:24:27 +0200

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -8,12 +8,11 @@ move $INSTALL StatisKit\doc\win_%INSTALL%
 cd StatisKit
 git config --global user.email %GIT_EMAIL%
 git config --global user.name %GIT_NAME%
-git add doc/win_%INSTALL%
+git add doc\win_%INSTALL%
 git commit -a -m "Update "win_%INSTALL%" script"
 echo machine github.com >>  %USERPROFILE%\_netrc
 echo login %GITHUB_USERNAME% >> %USERPROFILE%\_netrc
 echo password %GITHUB_PASSWORD% >>  %USERPROFILE%\_netrc
-type %USERPROFILE%\_netrc
 git push
 
 echo OFF
