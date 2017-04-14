9b12003540c6ea3bb2c4d07d6f2b422255c69099
Pierre Fernique <pfernique@gmail.com>
2017-04-14 10:40:07 +0200

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -1,21 +1,24 @@
 echo OFF
 
 git clone https://github.com/StatisKit/StatisKit.git
-if exist StatisKit\doc\win_%INSTALL%.exe (
-  del StatisKit\doc\win_%INSTALL%.exe
-)
 if "%PLATFORM%" == "x86" (
   set WIN=32
 ) else (
   set WIN=64
 )
-move %INSTALL%.exe StatisKit\doc\win%WIN%_%INSTALL%.exe
+if not exist StatisKit\doc\win mkdir StatisKit\doc\win
+if not exist StatisKit\doc\win\%WIN% mkdir StatisKit\doc\win\%WIN%
+if exist StatisKit\doc\win\%INSTALL%.exe (
+  del StatisKit\doc\win\%WIN%\%INSTALL%.exe
+)
+
+move %INSTALL%.exe StatisKit\doc\win\%WIN%\%INSTALL%.exe
 cd StatisKit
 git config --global user.email %GIT_EMAIL%
 git config --global user.name %GIT_NAME%
 dir doc
-git add doc\win%WIN%_%INSTALL%.exe
-git commit -a -m "Update "win%WIN%_%INSTALL%".exe script"
+git add doc\win\%WIN%\%INSTALL%.exe
+git commit -a -m "Update win/"%WIN%"/"%INSTALL%".exe script"
 echo machine github.com >>  %USERPROFILE%\_netrc
 echo login %GITHUB_USERNAME% >> %USERPROFILE%\_netrc
 echo password %GITHUB_PASSWORD% >>  %USERPROFILE%\_netrc
