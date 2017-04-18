e12c772b54d1af26cee2f18907688aecbb627f27
Pierre Fernique <pfernique@gmail.com>
2017-04-18 10:52:00 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,18 @@
 echo ON
 
-set BATCH_MODE=true
-set CONFIGURE_ONLY=true
-set CONDA_DIR=%HOMEDRIVE%\Conda
-curl https://raw.githubusercontent.com/StatisKit/StatisKit/master/doc/developer/developer_install.bat -o developer_install.bat
+if "%PLATFORM%" == "x86" (
+  set ARCH=32
+) else (
+  set ARCH=64
+)
+curl https://raw.githubusercontent.com/StatisKit/StatisKit/master/doc/win/%ARCH%/developer_install.exe -o developer_install.exe
 if errorlevel 1 exit 1
-call developer_install.bat
+developer_install.exe --prepend-path=no --configure-only=yes --prefix=%HOMEDRIVE\MinicondaSTK
 if errorlevel 1 exit 1
-echo ON
-del developer_install.bat
+del developer_install.exe
+if errorlevel 1 exit 1
+set PATH=%HOMEDRIVE%\MinicondaSTK;%HOMEDRIVE%\MinicondaSTK\Scripts;%PATH%
+if errorlevel 1 exit 1
 
 for /f %%i in ('python python_version.py') DO (set PYTHON_VERSION=%%i)
 if errorlevel 1 exit 1
