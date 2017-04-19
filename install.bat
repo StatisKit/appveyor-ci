25c1d50fe689eaadf1f5a1d3595b682ad4ac2fcd
Pierre Fernique <pfernique@gmail.com>
2017-04-19 07:22:24 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,13 +5,15 @@ if "%PLATFORM%" == "x86" (
 ) else (
   set ARCH=64
 )
+rmdir /s /q C:\Miniconda
+if errorlevel 1 exit 1
 curl https://raw.githubusercontent.com/StatisKit/StatisKit/master/doc/win/%ARCH%/developer_install.exe -o developer_install.exe
 if errorlevel 1 exit 1
-developer_install.exe --prepend-path=no --configure-only=yes --prefix=%HOMEDRIVE%\MinicondaSTK
+developer_install.exe --prepend-path=no --configure-only=yes --prefix=%HOMEDRIVE%\Miniconda
 if errorlevel 1 exit 1
 del developer_install.exe
 if errorlevel 1 exit 1
-set PATH=%HOMEDRIVE%\MinicondaSTK;%HOMEDRIVE%\MinicondaSTK\Scripts;%PATH%
+set PATH=%HOMEDRIVE%\Miniconda;%HOMEDRIVE%\Miniconda\Scripts;%PATH%
 if errorlevel 1 exit 1
 
 for /f %%i in ('python python_version.py') DO (set PYTHON_VERSION=%%i)
