3abb1bb271fb50cf0d861819a1a0f5386b8f5ad4
Pierre Fernique <pfernique@gmail.com>
2017-07-27 14:10:22 +0200

    Update build_script.bat

--- build_script.bat
+++ build_script.bat
@@ -1,7 +1,7 @@
 echo ON
 
 if not "%CONDA_RECIPE%"=="" conda build ..\bin\conda\%CONDA_RECIPE% -c statiskit -c conda-forge
-if not "%JUPYTER_NOTEBOOK%"=="" jupyter nbconvert --ExecutePreprocessor.timeout=3600 --to notebook --execute ..\share\%JUPYTER_NOTEBOOK% --output ..\share\%JUPYTER_NOTEBOOK%
+if not "%JUPYTER_NOTEBOOK%"=="" jupyter nbconvert --ExecutePreprocessor.timeout=3600 --to notebook --execute ..\share\jupyter\%JUPYTER_NOTEBOOK% --output ..\share\jupyter\%JUPYTER_NOTEBOOK%
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
