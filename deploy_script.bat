9de6fa48e2e48e2055aaae184ec853e01bd7c7e1
Pierre Fernique <pfernique@gmail.com>
2017-09-28 11:40:37 +0200

    Update deploy_script.bat

--- deploy_script.bat
+++ deploy_script.bat
@@ -3,7 +3,7 @@ echo ON
 set
 
 if exist ANACONDA_DEPLOY (
-  for /f %%i in ('conda build --python=%PYTHON_VERSION% ..\bin\conda\%CONDA_RECIPE% -c statiskit -c conda-forge --output') do anaconda upload %%i --user statiskit --force
+  for /f %%i in ('conda build --python=%MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% ..\bin\conda\%CONDA_RECIPE% -c statiskit -c conda-forge --output') do anaconda upload %%i --user statiskit --force
   if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
