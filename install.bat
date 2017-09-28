88679c1aecc60248abd37afbf7a0f1e7f253f54c
Pierre Fernique <pfernique@gmail.com>
2017-09-28 11:39:42 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -23,6 +23,19 @@ if errorlevel 1 exit 1
 for /f %%i in ('python python_version.py') DO (set PYTHON_VERSION=%%i)
 if errorlevel 1 exit 1
 
+set MAJOR_PYTHON_VERSION=%PYTHON_VERSION:~0,1%
+
+if "%PYTHON_VERSION:~2,1%" == "" (
+    :: PYTHON_VERSION style, such as 27, 34 etc.
+    set MINOR_PYTHON_VERSION=%PYTHON_VERSION:~1,1%
+) else (
+    if "%PYTHON_VERSION:~3,1%" == "." (
+     set MINOR_PYTHON_VERSION=%PYTHON_VERSION:~2,1%
+    ) else (
+     set MINOR_PYTHON_VERSION=%PYTHON_VERSION:~2,2%
+    )
+)
+
 set CMD_IN_ENV=cmd /E:ON /V:ON /C %cd%\\cmd_in_env.cmd
 if errorlevel 1 exit 1
 
