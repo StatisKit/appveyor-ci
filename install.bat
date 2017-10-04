eb4fd6937e44860ca659de2edea93076829bfd07
Pierre Fernique <pfernique@gmail.com>
2017-10-04 20:29:34 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,17 +7,26 @@ if "%CONDA_VERSION%" == "" (
 if "%PLATFORM%" == "x86" (
   set ARCH=32
 ) else (
-  set ARCH=64
+  set ARCH=x86_64
 )
+
 rmdir /s /q C:\Miniconda
 if errorlevel 1 exit 1
-curl https://raw.githubusercontent.com/StatisKit/install-binaries/master/win/%ARCH%/PY%CONDA_VERSION%/developer_install.exe -o developer_install.exe
+curl https://repo.continuum.io/miniconda/Miniconda%CONDA_VERSION%-latest-Windows-%ARCH%.exe -o miniconda.exe
 if errorlevel 1 exit 1
-developer_install.exe --prepend-path=no --configure-only=yes --prefix=%HOMEDRIVE%\Miniconda
+powershell Start-Process -FilePath miniconda.exe -ArgumentList /AddToPath=0,/InstallationType=JustMe,/RegisterPython=0,/S,/D=%HOMEDRIVE%\Miniconda -Wait
 if errorlevel 1 exit 1
-del developer_install.exe
+del miniconda.exe
 if errorlevel 1 exit 1
 set PATH=%HOMEDRIVE%\Miniconda;%HOMEDRIVE%\Miniconda\Scripts;%PATH%
+if errorlevel 1 exit 1
+source activate root
+if errorlevel 1 exit 1
+conda config --set always_yes yes
+if errorlevel 1 exit 1
+conda update conda
+if errorlevel 1 exit 1
+conda install conda-build anaconda-client
 if errorlevel 1 exit 1
 
 for /f %%i in ('python python_version.py') DO (set PYTHON_VERSION=%%i)
