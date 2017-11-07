b37ea51e391804fea0782d0f2484e0ce1661841a
Pierre Fernique <pfernique@gmail.com>
2017-11-07 15:41:31 +0100

    Update before_build.bat

--- before_build.bat
+++ before_build.bat
@@ -1,18 +1,10 @@
 echo ON
 
-if not "%CONDA_RECIPE%" == "" (
-    if exist ..\bin\conda\%CONDA_RECIPE%\appveyor-ci.patch (
-        cd ..\bin\conda\%CONDA_RECIPE%
-        git apply -v appveyor-ci.patch
-        if errorlevel neq 0 exit /b 1
-        cd ..\..\..\appveyor-ci
-    )
-)
-
 if not "%CONDA_ENVIRONMENT%" == "" (
-    conda create -n %CONDA_ENVIRONMENT% python=%CONDA_VERSION%
-    activate %CONDA_ENVIRONMENT%
-    conda env update -f ..\environment.yml
+    conda create -n appveyor-ci python=%CONDA_VERSION%
+    activate appveyor-ci 
+    conda env update -f ..\%CONDA_ENVIRONMENT%
+    activate appveyor-ci 
 )
 
 echo OFF
