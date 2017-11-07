6b8a5b2fd0c3fb70d6bd2be597d3df0e8e1dd0a6
Pierre Fernique <pfernique@gmail.com>
2017-11-07 16:17:52 +0100

    Update build_script.bat

--- build_script.bat
+++ build_script.bat
@@ -1,12 +1,17 @@
 echo ON
 
-if not "%CONDA_RECIPE%"=="" conda build --old-build-string --python=%MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% ..\bin\conda\%CONDA_RECIPE% -c statiskit -c conda-forge
+if not "%CONDA_RECIPE%"=="" (
+  conda build --old-build-string --python=%MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% ..\%CONDA_RECIPE%
+  if errorlevel 1 exit 1
+)
+
 if not "%JUPYTER_NOTEBOOK%"=="" (
   if not "%CONDA_ENVIRONMENT%"=="" (
-    activate %CONDA_ENVIRONMENT%
+    activate appveyor-ci
+    if errorlevel 1 exit 1
   )
-  jupyter nbconvert --ExecutePreprocessor.kernel_name="python%CONDA_VERSION%" --ExecutePreprocessor.timeout=3600 --to notebook --execute ..\share\jupyter\%JUPYTER_NOTEBOOK% --output %JUPYTER_NOTEBOOK%
+  jupyter nbconvert --ExecutePreprocessor.kernel_name="python%CONDA_VERSION%" --ExecutePreprocessor.timeout=0 --to notebook --execute ..\%JUPYTER_NOTEBOOK% --output ..\%JUPYTER_NOTEBOOK%
+  if errorlevel 1 exit 1
 )
-if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo OFF
