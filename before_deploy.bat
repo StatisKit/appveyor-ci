d7b8d2fd13098e7915db77b83e973a2a8c35de6a
Pierre Fernique <pfernique@gmail.com>
2017-11-07 17:07:30 +0100

    Update before_deploy.bat

--- before_deploy.bat
+++ before_deploy.bat
@@ -3,19 +3,20 @@ echo ON
 
 if not exist ANACONDA_DEPLOY (
   if exist CONDA_RECIPE (
-    set ANACONDA_DEPLOY=true
-    if "%ANACONDA_USERNAME%"=="" set ANACONDA_DEPLOY=false
-    if "%ANACONDA_PASSWORD%"=="" set ANACONDA_DEPLOY=false
+    if not "%ANACONDA_USERNAME%"=="" (
+      if not "%ANACONDA_PASSWORD%"=="" (
+        echo y|anaconda login --password %ANACONDA_PASSWORD% --username %ANACONDA_USERNAME%
+        if errorlevel 1 exit 1
+        set ANACONDA_DEPLOY=true
+      ) else (
+        set ANACONDA_DEPLOY=false
+      )
+    ) else (
+      set ANACONDA_DEPLOY=false
+    )
+  ) else (
+    set ANACONDA_DEPLOY=false
   )
-)
-
-echo !ANACONDA_DEPLOY!
-
-if "!ANACONDA_DEPLOY!"=="true" (
-  echo y|anaconda login --password %ANACONDA_PASSWORD% --username %ANACONDA_USERNAME%
-  if %errorlevel% neq 0 exit /b %errorlevel%
-  type NUL > ANACONDA_DEPLOY
-  if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
 echo OFF
