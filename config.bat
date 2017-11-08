bf945a263e34b894173e4318a1cdd99ea6f5cab4
Pierre Fernique <pfernique@gmail.com>
2017-11-08 10:22:17 +0100

    Update label channel

--- config.bat
+++ config.bat
@@ -10,12 +10,12 @@ if not "%ANACONDA_UPLOAD%" == "statiskit" (
     conda config --add channels %ANACONDA_UPLOAD%
     if errorlevel 1 exit 1
     if not "%ANACONDA_LABEL%" == "main" (
-      conda config --add channels %ANACONDA_UPLOAD%:%ANACONDA_LABEL%
+      conda config --add channels %ANACONDA_UPLOAD%/label/%ANACONDA_LABEL%
       if errorlevel 1 exit 1
     )
 ) else (
     if not "%ANACONDA_LABEL%" == "main" (
-      conda config --add channels statiskit:%ANACONDA_LABEL%
+      conda config --add channels statiskit/label/%ANACONDA_LABEL%
       if errorlevel 1 exit 1
     )
 )
