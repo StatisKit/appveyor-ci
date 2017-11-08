010a0398bff4683478feca24aa02889f01267a5c
Pierre Fernique <pfernique@gmail.com>
2017-11-08 10:07:48 +0100

    Add ANACONDA_LABEL in channels

--- config.bat
+++ config.bat
@@ -9,4 +9,13 @@ if errorlevel 1 exit 1
 if not "%ANACONDA_UPLOAD%" == "statiskit" (
     conda config --add channels %ANACONDA_UPLOAD%
     if errorlevel 1 exit 1
+    if not "%ANACONDA_LABEL%" == "main" (
+      conda config --add channels %ANACONDA_UPLOAD%:%ANACONDA_LABEL%
+      if errorlevel 1 exit 1
+    )
+) else (
+    if not "%ANACONDA_LABEL%" == "main" (
+      conda config --add channels statiskit:%ANACONDA_LABEL%
+      if errorlevel 1 exit 1
+    )
 )
