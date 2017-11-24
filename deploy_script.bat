7cbc5926df70240ae03b39dafb59ae9b96c47764
Pierre Fernique <pfernique@gmail.com>
2017-11-24 10:17:48 +0100

    Update deploy_script.bat

--- deploy_script.bat
+++ deploy_script.bat
@@ -28,5 +28,11 @@ if "%ANACONDA_DEPLOY%" == "true" (
         if errorlevel 1 exit 1
     )
 )
+if not "%ANACONDA_RELABEL%" == "" (
+    anaconda label -o %ANACONDA_UPLOAD% --copy %ANACONDA_LABEL% %ANACONDA_RELABEL%
+    if errorlevel 1 exit 1
+    anaconda label -o %ANACONDA_UPLOAD% --remove %ANACONDA_LABEL%
+    if errorlevel 1 exit 1
+)
 
 echo OFF
