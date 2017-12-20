53bf6f360ab231880a7149041d29158896e865b4
Pierre Fernique <pfernique@gmail.com>
2017-12-20 15:51:10 +0100

    Create release.py

---
+++ release.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import os
+
+headers = {"Content-Type": "application/json"}
+url = "https://ci.appveyor.com/api/projects/" + os.environ["APPVEYOR_PROJECT_SLUG"] + "/build/" + os.environ["APPVEYOR_BUILD_NUMBER"]
+status = requests.get(url, headers=headers).json()
+
+APPVEYOR_JOB_NUMBER = int(os.environ["APPVEYOR_JOB_NUMBER"])
+ARCH = os.environ["ARCH"]
+
+for job in status['build']['jobs'][:APPVEYOR_JOB_NUMBER]:
+    if not job["allowFailure"] and "Platform: " + ARCH in job["name"] and job["status"] == 'failed':
+        raise Exception("A previous job failed !")
