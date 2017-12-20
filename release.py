1b2d4c780ef75695e875c00f75073c91266173af
Pierre Fernique <pfernique@gmail.com>
2017-12-21 00:45:44 +0100

    Update release.py

--- release.py
+++ release.py
@@ -3,7 +3,7 @@ import json
 import os
 
 headers = {"Content-Type": "application/json"}
-url = "https://ci.appveyor.com/api/projects/" + os.environ["APPVEYOR_PROJECT_SLUG"] + "/build/" + os.environ["APPVEYOR_BUILD_NUMBER"]
+url = "https://ci.appveyor.com/api/projects/" + os.environ["APPVEYOR_PROJECT_SLUG"] + "/build/" + os.environ["APPVEYOR_BUILD_VERSION"] + "." + os.environ["APPVEYOR_BUILD_NUMBER"]
 print(url)
 status = requests.get(url, headers=headers).json()
 
