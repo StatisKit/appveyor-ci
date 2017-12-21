83540da48f562ec9dac72c7205f14091284b9223
Pierre Fernique <pfernique@gmail.com>
2017-12-21 08:26:05 +0100

    Update release.py

--- release.py
+++ release.py
@@ -8,7 +8,9 @@ status = requests.get(url, headers=headers).json()
 
 APPVEYOR_JOB_NUMBER = int(os.environ["APPVEYOR_JOB_NUMBER"])
 ARCH = os.environ["ARCH"]
-
+if ARCH == 'x86_64':
+    ARCH = 'x64'
+    
 for job in status['build']['jobs'][:APPVEYOR_JOB_NUMBER]:
     if not job["allowFailure"] and "Platform: " + ARCH in job["name"] and job["status"] == 'failed':
         raise Exception("A previous job failed !")
