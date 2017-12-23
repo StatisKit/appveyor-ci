6134ca60841eb86e4ad129ce4887f87c9d539f6d
Pierre Fernique <pfernique@gmail.com>
2017-12-23 11:13:26 +0100

    Update deploy_script.bat

--- deploy_script.bat
+++ deploy_script.bat
@@ -24,7 +24,7 @@ echo ON
 
 if "%ANACONDA_DEPLOY%" == "true" (
     if not "%CONDA_RECIPE%" == "" (
-        for /f %%i in ('conda build --old-build-string --python=%MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% ..\%CONDA_RECIPE% --output') do anaconda upload %%i --user %ANACONDA_UPLOAD% --force --label %ANACONDA_LABEL%
+        for /f %%i in ('conda build --old-build-string --python=%CONDA_VERSION% ..\%CONDA_RECIPE% --output') do anaconda upload %%i --user %ANACONDA_UPLOAD% --force --label %ANACONDA_LABEL%
         if errorlevel 1 exit 1
     )
 )
