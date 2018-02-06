59957290b1949a39a4140e146a5a4647d84ec32c
Pierre Fernique <pfernique@gmail.com>
2018-02-06 07:25:23 +0100

    Update release.py

--- release.py
+++ release.py
@@ -11,6 +11,11 @@ ARCH = os.environ["ARCH"]
 if ARCH == 'x86_64':
     ARCH = 'x64'
     
-for job in status['build']['jobs'][:APPVEYOR_JOB_NUMBER]:
-    if not job["allowFailure"] and "Platform: " + ARCH in job["name"] and job["status"] == 'failed':
-        raise Exception("A previous job failed !")
+if all("Platform: " in in job["name"] for job in status['build']['jobs']):
+    for job in status['build']['jobs'][:APPVEYOR_JOB_NUMBER]:
+        if not job["allowFailure"] and "Platform: " + ARCH in job["name"] and job["status"] == 'failed':
+            raise Exception("A previous job failed !")
+else:
+    for job in status['build']['jobs'][:APPVEYOR_JOB_NUMBER]:
+        if not job["allowFailure"] and job["status"] == 'failed':
+            raise Exception("A previous job failed !")
