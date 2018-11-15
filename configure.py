4a0242c385f7f1eac45911e56f0f3f8fa1ed0ae1
Pierre <pfernique@gmail.com>
2018-11-15 09:38:45 +0100

    Update

--- configure.py
+++ configure.py
@@ -1,9 +1,15 @@
 import os
-import six
 import platform
 import sys
 
-if six.PY2:
+if sys.version_info[0] == 2:
+    PY2 = True
+    PY3 = False
+else:
+    PY3 = True
+    PY2 = False
+
+if PY2:
     environ = {key : value for key, value in os.environ.iteritems() if value}
 else:
     environ = {key : value for key, value in os.environ.items() if value}
@@ -45,23 +51,6 @@ def get_anaconda_label():
     else:
         return "develop"
 
-def get_docker_owner():
-    if "DOCKER_LOGIN" in environ:
-        return environ["DOCKER_LOGIN"]
-
-def get_docker_deploy():
-    if "DOCKER_LOGIN" in environ and environ["TRAVIS_OS_NAME"] == "linux":
-        return environ["CI"]
-    else:
-        return "false"
-
-def get_docker_container():
-    if "DOCKER_CONTEXT" in environ:
-        return os.path.basename(environ["DOCKER_CONTEXT"])
-
-def get_appveyor_repo_tag_name():
-    return "latest"
-
 def get_jupyter_kernel():
     return "python" + environ["CONDA_VERSION"]
 
@@ -88,9 +77,12 @@ def get_old_build_string():
 
 def get_anaconda_tmp_label():
     if environ["ANACONDA_LABEL"] == "release":
-        return environ["TRAVIS_OS_NAME"] + "-" + environ["ARCH"] + "_release"
+        return "win-" + environ["ARCH"] + "_release"
     else:
         return environ["ANACONDA_LABEL"]
+
+def get_conda_prefix():
+    return "\%HOMEPATH\%\\miniconda"
 
 def main():
     for key in ["APPVEYOR_SCHEDULED_BUILD",
@@ -102,9 +94,6 @@ def main():
                 "ANACONDA_OWNER",
                 "ANACONDA_DEPLOY",
                 "ANACONDA_RELEASE",
-                "DOCKER_OWNER",
-                "DOCKER_CONTAINER",
-                "APPVEYOR_REPO_TAG_NAME",
                 "JUPYTER_KERNEL",
                 "PYTHON_VERSION",
                 "ANACONDA_FORCE", 
@@ -115,6 +104,19 @@ def main():
             value = eval("get_" + key.lower() + "()")
             if value:
                 environ[key] = value
+    ANACONDA_CHANNELS = []
+    if "ANACONDA_OWNER" in environ:
+        ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"])
+        if not environ["ANACONDA_LABEL"] == "main":
+            if not environ["ANACONDA_LABEL"] == "develop":
+                ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"] + "/label/develop")
+            ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"] + "/label/" + environ["ANACONDA_LABEL"])
+        if not environ["ANACONDA_TMP_LABEL"] == environ["ANACONDA_LABEL"]:
+            ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"] + "/label/" + environ["ANACONDA_TMP_LABEL"])
+    environ["ANACONDA_CHANNELS"] = ""
+    for ANACONDA_CHANNEL in list(reversed(environ.get("ANACONDA_CHANNELS", "").split(" "))) + ANACONDA_CHANNELS:
+        if ANACONDA_CHANNEL:
+            environ["ANACONDA_CHANNELS"] += " --add channels " + ANACONDA_CHANNEL
     if environ["ANACONDA_LABEL"] == "release":
         if environ["TRAVIS_EVENT_TYPE"] == "cron":
             environ["ANACONDA_LABEL"] = "cron"
@@ -128,25 +130,18 @@ def main():
         environ["OLD_BUILD_STRING"] = "--old-build-string"
     else:
         environ["OLD_BUILD_STRING"] = ""
-    ANACONDA_CHANNELS = []
-    if "ANACONDA_OWNER" in environ:
-        ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"])
-        if not environ["ANACONDA_LABEL"] == "main":
-            ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"] + "/label/" + environ["ANACONDA_LABEL"])
-        if not environ["ANACONDA_TMP_LABEL"] == environ["ANACONDA_LABEL"]:
-            ANACONDA_CHANNELS.append(environ["ANACONDA_OWNER"] + "/label/" + environ["ANACONDA_TMP_LABEL"])
-    ANACONDA_CHANNELS.reverse()
-    environ["ANACONDA_CHANNELS"] = " ".join(ANACONDA_CHANNELS + environ.get("ANACONDA_CHANNELS", "").split(" "))
     with open("configure.bat", "w") as filehandler:
         filehandler.write("echo ON\n\n")
-        if six.PY2:
+        if PY2:
             for key, value in environ.iteritems():
                 if key not in os.environ or not os.environ[key] == environ[key]:
                     filehandler.write("set " + key + "=\"" + value + "\"\n")
+                    filehandler.write("if errorlevel 1 exit 1\n")
         else:
             for key, value in environ.items():
                 if key not in os.environ or not os.environ[key] == environ[key]:
                     filehandler.write("set " + key + "=\"" + value + "\"\n")
+                    filehandler.write("if errorlevel 1 exit 1\n")
         filehandler.write("\necho OFF")
 
 if __name__ == "__main__":
