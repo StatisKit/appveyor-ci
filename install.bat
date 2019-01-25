7abac076dbe60488d6fcf5539237098c86ca137d
Pierre Fernique <pfernique@gmail.com>
2019-01-25 19:50:47 +0100

    Update

--- install.bat
+++ install.bat
@@ -20,7 +20,7 @@
 :: mplied. See the License for the specific language governing           ::
 :: permissions and limitations under the License.                        ::
 
-echo OFF
+echo ON
 
 call environ.bat
 
